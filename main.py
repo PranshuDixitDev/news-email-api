12c59dc92b20f0566fd44c377f2d44f0aead4ee7
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-03-25 11:21:29 +0530

    Basic Structure of news to send via email done.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import requests
+from send_email_news import send_email
 
-url = "https://finance.yahoo.com/"
+api_key = "1c431b8cedcb41b38c1bebca567a6a86"
+# url = "https://newsapi.org/v2/everything? \
+# 	q=dell& \
+# 	sortBy=publishedAt& \
+# 	apiKey=1c431b8cedcb41b38c1bebca567a6a86 \
+# 	language=en"
+
+url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1c431b8cedcb41b38c1bebca567a6a86&language=en"
 
 request = requests.get(url)
-content = request.text
+content = request.json()
 
-print(content)
+
+body = ""
+for article in content["articles"]:
+	if article["title"] and article["description"] is not None:
+		body = body + article["title"] + "\n" + article["description"] + article["url"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
