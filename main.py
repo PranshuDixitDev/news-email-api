79cf88afa39660a6c6de16b0551c5d9c5ada0c79
PranshuDixitDev <pranshudixit.dev@yahoo.com>
2024-03-25 11:57:42 +0530

    In that structure we added links, specified languages; This commit we limit new , add subject , control category.

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import requests
 from send_email_news import send_email
 
+topic = "tesla"
 api_key = "1c431b8cedcb41b38c1bebca567a6a86"
-# url = "https://newsapi.org/v2/everything? \
-# 	q=dell& \
-# 	sortBy=publishedAt& \
-# 	apiKey=1c431b8cedcb41b38c1bebca567a6a86 \
-# 	language=en"
+url = "https://newsapi.org/v2/everything?" \
+      "q={}&" \
+      "sortBy=publishedAt&" \
+      "apiKey={}&" \
+      "language=en".format(topic, api_key)
 
-url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1c431b8cedcb41b38c1bebca567a6a86&language=en"
+
+
+# url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=1c431b8cedcb41b38c1bebca567a6a86&language=en"
 
 request = requests.get(url)
 content = request.json()
 
 
-body = ""
-for article in content["articles"]:
-	if article["title"] and article["description"] is not None:
-		body = body + article["title"] + "\n" + article["description"] + article["url"] + 2*"\n"
+body = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
+    if (article["description"] is not None) and (article["title"] is not None):
+        body = body + article["title"] + "\n"\
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
